25033f2fcdfb9324f4972f38191a64e731f1d7b6
sujoy-behera <beherasujoy@gmail.com>
2026-08-08 21:34:24 +0530

    question 2 typing accuracy checker using concepts String traversal, charAt(), loops, conditional logic, percentage calculation.

--- src/Week1/Question1_DuplicateSeatChecker.java
+++ src/Week1/Question1_DuplicateSeatChecker.java
@@ -1,14 +1,10 @@
 package Week1;
 
 public class Question1_DuplicateSeatChecker {
-
-
     static void checkDuplicateSeats(int[] seatNumbers) {
         boolean duplicateFound = false;
-
         for (int i = 0; i < seatNumbers.length; i++) {
             for (int j = i + 1; j < seatNumbers.length; j++) {
-
                 if (seatNumbers[i] == seatNumbers[j]) {
                     System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
                     duplicateFound = true;
@@ -16,12 +12,10 @@ public class Question1_DuplicateSeatChecker {
                 }
             }
         }
-
         if (!duplicateFound) {
             System.out.println("No Duplicate Seats Found");
         }
     }
-
     public static void main(String[] args) {
 
         int[] seatNumbers = {101, 102, 103, 102, 105};
