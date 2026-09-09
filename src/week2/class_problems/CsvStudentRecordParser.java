075ec274168263033c6ce0d9e686798f287c53c1
Sujoy Behera <beherasujoy@gmail.com>
2026-09-09 15:59:01 +0530

    Week 2 Session 2: Add CSV student record parser

---
+++ src/week2/class_problems/CsvStudentRecordParser.java
@@ -0,0 +1,20 @@
+package week2.class_problems;
+
+import java.util.Scanner;
+
+public class CsvStudentRecordParser {
+    static void parseStudentRecord(String csvLine) {
+        String[] fields = csvLine.split(",");
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+        System.out.println("Name: " + fields[0] + " | Roll No: " + fields[1] + " | Dept: " + fields[2]);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter student record: ");
+        parseStudentRecord(scanner.nextLine());
+    }
+}
