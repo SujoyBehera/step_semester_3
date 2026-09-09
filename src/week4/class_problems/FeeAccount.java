8a7e0d91a61f4e97ee7ecc4a8be3cb787d7f2819
Sujoy Behera <beherasujoy@gmail.com>
2026-09-09 18:21:58 +0530

    Week 4 Session 4: Add account batch payments classwork

---
+++ src/week4/class_problems/FeeAccount.java
@@ -0,0 +1,46 @@
+package week4.class_problems;
+
+public class FeeAccount {
+    private String type;
+
+    public FeeAccount(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static void processPayment(FeeAccount account, double amount) {
+        if (account instanceof HostelFeeAccount) {
+            System.out.println("Paid in two installments (hostel account)");
+        } else {
+            System.out.println("Paid in one go (day-scholar account)");
+        }
+    }
+
+    public static void main(String[] args) {
+        FeeAccount[] accounts = {
+            new HostelFeeAccount(), new HostelFeeAccount(),
+            new FeeAccount("Day Scholar"), new FeeAccount("Day Scholar")
+        };
+        double amount = 60000;
+        int hostelCount = 0;
+        int dayScholarCount = 0;
+
+        for (FeeAccount account : accounts) {
+            processPayment(account, amount);
+            if (account instanceof HostelFeeAccount) hostelCount++;
+            else dayScholarCount++;
+        }
+
+        System.out.println("Hostel accounts processed: " + hostelCount
+                + " | Day-scholar accounts processed: " + dayScholarCount);
+    }
+}
+
+class HostelFeeAccount extends FeeAccount {
+    public HostelFeeAccount() {
+        super("Hostel");
+    }
+}
