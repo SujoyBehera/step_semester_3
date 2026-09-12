24182246252fa15afd00cd9f71f318bdd7e5698d
Sujoy Behera <beherasujoy@gmail.com>
2026-09-13 03:16:58 +0530

    Add Week 5 assignment solution - LibraryMember

---
+++ src/week5/assigment_problems/LibraryMember.java
@@ -0,0 +1,48 @@
+package week5.assigment_problems;
+
+public class LibraryMember {
+    private String membershipPin;
+    String branchCode;
+    protected double finesOwed;
+    public String displayName;
+
+    private String membershipId;
+    private String name;
+    private boolean premiumMember;
+    private String securityAnswer;
+
+    public LibraryMember() {
+    }
+
+    public String getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(String id) {
+        if (membershipId == null) membershipId = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPremiumMember() {
+        return premiumMember;
+    }
+
+    public void setPremiumMember(boolean premium) {
+        this.premiumMember = premium;
+    }
+
+    public void setSecurityAnswer(String answer) {
+        if (answer == null) {
+            securityAnswer = null;
+            return;
+        }
+        securityAnswer = Integer.toHexString(answer.hashCode());
+    }
+}
