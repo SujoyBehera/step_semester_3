90d4045ef322216f3bf5ca7d78fe89be27711649
Sujoy Behera <beherasujoy@gmail.com>
2026-09-13 03:16:54 +0530

    Add Week 5 assignment solution - BookInventory

---
+++ src/week5/assigment_problems/BookInventory.java
@@ -0,0 +1,26 @@
+package week5.assigment_problems;
+
+public class BookInventory {
+    private final int copiesTotal;
+    private int copiesAvailable;
+
+    public BookInventory(int copiesTotal) {
+        if (copiesTotal <= 0) {
+            throw new IllegalArgumentException("copiesTotal must be positive");
+        }
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+    public void checkOut() {
+        if (copiesAvailable > 0) copiesAvailable--;
+    }
+
+    public void checkIn() {
+        if (copiesAvailable < copiesTotal) copiesAvailable++;
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+}
