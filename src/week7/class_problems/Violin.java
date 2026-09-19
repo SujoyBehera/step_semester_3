e6131a5c49eb419fa18123406446dd95c3573a01
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:34:07 +0530

    Week 7 practice: add Violin.java

---
+++ src/week7/class_problems/Violin.java
@@ -0,0 +1,5 @@
+package week7.class_problems;
+public class Violin extends StringInstrument {
+    public Violin(){ super(); }
+    public String play(){ return super.play()+", with a bow drawn across four strings"; }
+}
