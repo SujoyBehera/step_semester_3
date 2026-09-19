d656f4ef136ffa5c7abce2d40ce9da9c0a849cdc
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:44:01 +0530

    Week 7: keep one code file per assignment question

--- src/week7/assigment_problems/ScoutDrone.java
+++
@@ -1,5 +0,0 @@
-package week7.assigment_problems;
-public class ScoutDrone extends Drone {
-    public ScoutDrone(String id){ super(id); }
-    public String fly(){ return getId()+" scouting the area"; }
-}
