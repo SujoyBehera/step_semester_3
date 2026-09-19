c25da2d97f1ccec86e9412618cccee37b705d8b8
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:35:31 +0530

    Week 7: add Pruner.java

---
+++ src/week7/assigment_problems/Pruner.java
@@ -0,0 +1,5 @@
+package week7.assigment_problems;
+public class Pruner extends CuttingTool {
+    public Pruner(){ super(); }
+    public String use(){ return super.use()+", then trimming branches precisely"; }
+}
