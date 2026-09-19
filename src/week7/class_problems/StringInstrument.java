fdba2e6a2e9912f882d07096b0a8fd8fabcd489a
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:43:10 +0530

    Week 7: keep one code file per practice question

--- src/week7/class_problems/StringInstrument.java
+++
@@ -1,5 +0,0 @@
-package week7.class_problems;
-public class StringInstrument extends Instrument {
-    public StringInstrument(){ super(); }
-    public String play(){ return "Strumming the strings"; }
-}
