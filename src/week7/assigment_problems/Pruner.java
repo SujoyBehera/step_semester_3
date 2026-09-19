1a058e0fc1b54dbd474b03b3245016b0be5f9e49
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:43:39 +0530

    Week 7: keep one code file per assignment question

--- src/week7/assigment_problems/Pruner.java
+++
@@ -1,5 +0,0 @@
-package week7.assigment_problems;
-public class Pruner extends CuttingTool {
-    public Pruner(){ super(); }
-    public String use(){ return super.use()+", then trimming branches precisely"; }
-}
