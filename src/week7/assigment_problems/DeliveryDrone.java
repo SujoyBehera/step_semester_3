3f1cc34272a118d3a083d665b1c6bbc261da4f36
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:43:57 +0530

    Week 7: keep one code file per assignment question

--- src/week7/assigment_problems/DeliveryDrone.java
+++
@@ -1,6 +0,0 @@
-package week7.assigment_problems;
-public class DeliveryDrone extends Drone implements Trackable {
-    public DeliveryDrone(String id){ super(id); }
-    public String fly(){ return getId()+" delivering package"; }
-    public String getLocation(){ return getId()+" at Sector 4"; }
-}
