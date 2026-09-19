7555f77b640993faa8e9304589fdbea301b852a3
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:43:37 +0530

    Week 7: keep one code file per assignment question

--- src/week7/assigment_problems/CuttingTool.java
+++
@@ -1,5 +0,0 @@
-package week7.assigment_problems;
-public class CuttingTool extends GardenTool {
-    public CuttingTool(){ super(); }
-    public String use(){ return super.use()+", blade sharpened first"; }
-}
