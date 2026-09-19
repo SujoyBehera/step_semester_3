3edad906745380f6ac9229c309c44f1c6612ac2b
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:34:26 +0530

    Week 7: add DeliveryNote.java

---
+++ src/week7/class_problems/DeliveryNote.java
@@ -0,0 +1,5 @@
+package week7.class_problems;
+public abstract class DeliveryNote {
+    public abstract String confirmDelivery();
+    public String confirmDelivery(String signature){ return confirmDelivery()+", signed by "+signature; }
+}
