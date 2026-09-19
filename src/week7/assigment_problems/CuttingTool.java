58cca06a8430a4e21309b12d4055f77b30e7b99d
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:35:28 +0530

    Week 7: add CuttingTool.java

---
+++ src/week7/assigment_problems/CuttingTool.java
@@ -0,0 +1,5 @@
+package week7.assigment_problems;
+public class CuttingTool extends GardenTool {
+    public CuttingTool(){ super(); }
+    public String use(){ return super.use()+", blade sharpened first"; }
+}
