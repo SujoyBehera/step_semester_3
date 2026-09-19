d167d08495b8bdd0f6d0adc58c36ef0c18d0c4ea
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:44:11 +0530

    Week 7: keep one code file per assignment question

--- src/week7/assigment_problems/TrackingOperations.java
+++
@@ -1,8 +0,0 @@
-package week7.assigment_problems;
-public final class TrackingOperations {
-    private TrackingOperations(){}
-    public static String getLocationIfTrackable(Object object){
-        if(object instanceof Trackable) return ((Trackable)object).getLocation();
-        return "Tracking not available";
-    }
-}
