8e46023a33e8f34dd680c95fa53381e32e8134e6
Sujoy Behera <beherasujoy@gmail.com>
2026-09-19 18:41:46 +0530

    Week 7: consolidate practice problem into one code file

--- src/week7/class_problems/Instrument.java
+++ src/week7/class_problems/Instrument.java
@@ -1,2 +1,13 @@
 package week7.class_problems;
+
 public abstract class Instrument { public abstract String play(); }
+
+class StringInstrument extends Instrument {
+    public StringInstrument() { super(); }
+    @Override public String play() { return "Strumming the strings"; }
+}
+
+class Violin extends StringInstrument {
+    public Violin() { super(); }
+    @Override public String play() { return super.play() + ", with a bow drawn across four strings"; }
+}
